343f671e73d99ae0596afd67cc8935a66dd0d145
Alana Sampaio <alana.sampaio2000@gmail.com>
2023-12-13 04:04:45 -0300

    Ajuste da chave assimétrica e adição de comentários

--- P02.2CriptografiaComBug/chat2.py
+++ P02.2CriptografiaComBug/chat2.py
@@ -7,42 +7,74 @@ from cryptography.hazmat.primitives.asymmetric import rsa, padding
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.backends import default_backend
 
+# Porta padrão para a comunicação UDP. Todos os peers devem ouvir nesta porta.
 numIp = 12345
+
 class LamportClock:
+    """
+    Classe para implementar um relógio de Lamport.
+    O relógio de Lamport é um mecanismo para determinar a ordem dos eventos em um sistema distribuído,
+    essencial para manter a consistência do estado entre diferentes partes do sistema.
+    """
     def __init__(self):
+        # Inicializa o valor do relógio para 0.
         self.value = 0
+        # Lock para garantir que as operações com o relógio sejam thread-safe.
         self.lock = threading.Lock()
 
     def increment(self):
+        """
+        Incrementa o valor do relógio.
+        Isso é feito antes de um peer enviar uma mensagem, para garantir a ordem das mensagens.
+        """
         with self.lock:
             self.value += 1
             return self.value
 
     def update(self, received_time):
+        """
+        Atualiza o valor do relógio com base no tempo recebido de outra mensagem.
+        Isso ajuda a manter a consistência da ordem dos eventos entre diferentes peers.
+        """
         with self.lock:
             self.value = max(self.value, received_time) + 1
             return self.value
 
+# Gerador de chaves RSA para criptografia assimétrica.
 def generate_keys():
+    """
+    Gera um par de chaves RSA - uma chave privada e uma chave pública.
+    A chave pública pode ser compartilhada com outros peers para permitir que eles criptografem mensagens.
+    A chave privada é mantida em segredo e usada para descriptografar mensagens recebidas.
+    """
     private_key = rsa.generate_private_key(
-        public_exponent=65537,
-        key_size=2048,
-        backend=default_backend()
+        public_exponent=65537,  # Exponente público comum para chaves RSA.
+        key_size=2048,  # Tamanho da chave. Quanto maior, mais segura, mas também mais lenta.
+        backend=default_backend()  # Backend criptográfico para a geração da chave.
     )
-    public_key = private_key.public_key()
+    public_key = private_key.public_key()  # Deriva a chave pública a partir da chave privada.
     return private_key, public_key
 
-
+# Serializador de chave pública.
 def serialize_public_key(public_key):
+    """
+    Serializa a chave pública para um formato que pode ser facilmente compartilhado (PEM).
+    Isso é útil para enviar a chave pública para outros peers.
+    """
     return public_key.public_bytes(
         encoding=serialization.Encoding.PEM,
         format=serialization.PublicFormat.SubjectPublicKeyInfo
     )
 
+# Funções de criptografia e descriptografia.
 def encrypt_message(message, public_key):
+    """
+    Criptografa uma mensagem usando a chave pública de um receptor.
+    Isso garante que apenas o receptor (que possui a chave privada correspondente) possa ler a mensagem.
+    """
     encrypted = public_key.encrypt(
-        message.encode(),
-        padding.OAEP(
+        message.encode(),  # Codifica a mensagem em bytes.
+        padding.OAEP(  # Padding OAEP para a criptografia RSA.
             mgf=padding.MGF1(algorithm=hashes.SHA256()),
             algorithm=hashes.SHA256(),
             label=None
@@ -51,70 +83,116 @@ def encrypt_message(message, public_key):
     return encrypted
 
 def decrypt_message(encrypted_message, private_key):
+    """
+    Descriptografa uma mensagem recebida usando a chave privada do receptor.
+    Isso é usado para ler mensagens que foram criptografadas com a chave pública do receptor.
+    """
     original_message = private_key.decrypt(
-        encrypted_message,
-        padding.OAEP(
+        encrypted_message,  # Mensagem criptografada.
+        padding.OAEP(  # Padding OAEP para a descriptografia RSA.
             mgf=padding.MGF1(algorithm=hashes.SHA256()),
             algorithm=hashes.SHA256(),
             label=None
         )
     )
-    return original_message.decode()
+    return original_message.decode()  # Converte a mensagem de volta para string.
 
+# Thread para escutar mensagens.
 def listen_for_messages(sock, clock, alias, my_private_key):
+    """
+    Thread que fica ouvindo por mensagens de outros peers.
+    Quando uma mensagem é recebida, ela é descriptografada e exibida para o usuário.
+    """
     while True:
         try:
-            data, addr = sock.recvfrom(1024)
-            encrypted_message, received_time = pickle.loads(data)
-            message_data = decrypt_message(encrypted_message, my_private_key)
-            message = json.loads(message_data)
-            clock.update(received_time)
+                       # Recebe dados de outros usuários. 'addr' contém o endereço do remetente.
+            data, addr = sock.recvfrom(1024)  
+            # Desempacota a mensagem criptografada e o tempo do relógio de Lamport.
+            encrypted_message, received_time = pickle.loads(data)  
+            # Descriptografa a mensagem usando a chave privada.
+            message_data = decrypt_message(encrypted_message, my_private_key)  
+            # Converte a mensagem de JSON para um dicionário Python.
+            message = json.loads(message_data)  
+            # Atualiza o relógio de Lamport com base no tempo recebido.
+            clock.update(received_time)  
+            # Exibe a mensagem descriptografada e o tempo do relógio de Lamport.
             print(f"{message['alias']} disse: {message['text']} at Lamport time {clock.value}")
         except Exception as e:
+            # Imprime qualquer erro que ocorrer durante a recepção e processamento da mensagem.
             print(f"Erro ao receber mensagem: {e}")
 
 def send_message(sock, clock, alias, peers, my_private_key, public_keys):
+    """
+    Função para enviar mensagens para outros peers. A mensagem é criptografada com a chave pública do destinatário.
+    """
     while True:
-        message_text = input("Digite sua mensagem: ")
-        lamport_time = clock.increment()
+        # Solicita ao usuário uma mensagem para enviar.
+        message_text = input("Digite sua mensagem: ")  
+        # Incrementa o relógio de Lamport antes de enviar a mensagem.
+        lamport_time = clock.increment()  
+        # Prepara os dados da mensagem para envio, convertendo para formato JSON.
         message_data = json.dumps({"alias": alias, "text": message_text})
 
+        # Envia a mensagem para cada um dos peers.
         for peer_alias in peers:
+            # Pega a chave pública do peer destinatário.
             public_key = public_keys[peer_alias]
+            # Criptografa a mensagem com a chave pública do destinatário.
             encrypted_data = encrypt_message(message_data, public_key)
+            # Empacota a mensagem criptografada e o tempo do relógio para envio.
             data = pickle.dumps((encrypted_data, lamport_time))
+            # Pega o endereço IP e porta do peer destinatário.
             peer_ip, peer_port = peers[peer_alias]
+            # Envia a mensagem criptografada para o peer.
             sock.sendto(data, (peer_ip, peer_port))
 
 def main():
-    
+    """
+    Função principal para executar o chat.
+    Configura o chat, incluindo geração de chaves, criação de socket e início das threads de escuta e envio.
+    """
+    # Solicita ao usuário um apelido para o chat.
     alias = input("Digite seu apelido no chat: ")
 
+    # Gera um par de chaves RSA para o usuário (chave privada e pública).
     my_private_key, my_public_key = generate_keys()
+    # Exibe a chave pública do usuário para compartilhamento com outros peers.
     print("Sua chave pública:")
     print(serialize_public_key(my_public_key).decode())
 
+    # Dicionário para armazenar os endereços dos peers e suas chaves públicas.
     peers = {}
     public_keys = {}
+    # Pede ao usuário o número de membros no chat.
     num = int(input("Digite o número de membros que terá seu chat em grupo (sem incluir você): "))
     for _ in range(num):
+        # Pede informações de cada membro (apelido, IP e chave pública).
         member_alias = input("Digite o apelido do membro: ")
         ip = input("Digite o IP do membro: ")
+        # Armazena o endereço IP e porta do membro no dicionário de peers.
         peers[member_alias] = (ip, numIp)
 
+        # Pede e armazena a chave pública de cada membro.
         public_key_pem = input(f"Digite a chave pública do membro {member_alias}: ")
+        # Converte a chave pública de formato PEM para um objeto de chave pública.
         public_key = serialization.load_pem_public_key(public_key_pem.encode())
+        # Armazena a chave pública no dicionário de chaves públicas.
         public_keys[member_alias] = public_key
 
+    # Configura um socket UDP para comunicação.
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    # Associa o socket à porta especificada.
     sock.bind(('', numIp))
 
+    # Cria uma instância do relógio de Lamport.
     clock = LamportClock()
-
+        # Inicia uma thread para ouvir mensagens de entrada.
     listener_thread = threading.Thread(target=listen_for_messages, args=(sock, clock, alias, my_private_key), daemon=True)
     listener_thread.start()
 
+    # Chama a função para enviar mensagens. Esta função continuará em execução e permitirá que o usuário
+    # digite e envie mensagens.
     send_message(sock, clock, alias, peers, my_private_key, public_keys)
 
 if __name__ == "__main__":
-    main()
+    main()  # Executa a função principal se o script for o módulo principal.
--- P02.2HashComum/chat3.py
+++ P02.2HashComum/chat3.py
@@ -2,84 +2,159 @@ import socket
 import threading
 import json
 import pickle
+import re
+import hashlib
+import base64
 from cryptography.fernet import Fernet
 
+# Define a porta padrão para a comunicação UDP.
 numIp = 12345
+
 class LamportClock:
+    """
+    Classe que implementa um relógio de Lamport, usado para estabelecer uma ordem de eventos
+    em um sistema distribuído, crucial para a consistência de estado entre diferentes nós.
+    """
     def __init__(self):
+        # Valor inicial do relógio.
         self.value = 0
+        # Lock para sincronização em ambientes multithread.
         self.lock = threading.Lock()
 
     def increment(self):
+        """
+        Incrementa o valor do relógio. Isso é feito antes de um nó enviar uma mensagem,
+        para assegurar a ordem das mensagens.
+        """
         with self.lock:
             self.value += 1
             return self.value
 
     def update(self, received_time):
+        """
+        Atualiza o relógio com o maior valor entre o atual e o recebido, e incrementa em 1.
+        Isso ajuda a manter a consistência da ordem dos eventos entre diferentes nós.
+        """
         with self.lock:
             self.value = max(self.value, received_time) + 1
             return self.value
-    
-def generate_key():
-    return Fernet.generate_key()
+
+def validate_key(key):
+    """
+    Valida se a chave de criptografia fornecida pelo usuário atende aos requisitos mínimos.
+    Isso inclui ter pelo menos 8 caracteres, com letras maiúsculas e minúsculas, números e
+    caracteres especiais.
+    """
+    if (len(key) >= 8 and
+        re.search("[a-z]", key) and
+        re.search("[A-Z]", key) and
+        re.search("[0-9]", key) and
+        re.search("[!@#$%^&*(),.<>/?]", key)):
+        return True
+    return False
+
+def generate_fernet_key_from_input(input_key):
+    """
+    Gera uma chave Fernet válida a partir da entrada do usuário. Isso é feito usando
+    um hash SHA-256 da entrada, que é então codificado em base64.
+    """
+    hash_key = hashlib.sha256(input_key.encode()).digest()
+    fernet_key = base64.urlsafe_b64encode(hash_key)
+    return fernet_key
 
 def encrypt_message(key, message):
+    """
+    Criptografa uma mensagem usando a chave Fernet.
+    Isso garante que apenas pessoas com a chave correta possam ler a mensagem.
+    """
     fernet = Fernet(key)
     return fernet.encrypt(message.encode())
 
 def decrypt_message(key, encrypted_message):
+    """
+    Descriptografa uma mensagem usando a chave Fernet.
+    Isso é usado para ler mensagens que foram criptografadas com a mesma chave.
+    """
     fernet = Fernet(key)
     return fernet.decrypt(encrypted_message).decode()
 
 def listen_for_messages(sock, clock, alias, key):
+    """
+    Thread que fica ouvindo por mensagens de outros usuários.
+    Quando uma mensagem é recebida, ela é descriptografada e exibida para o usuário.
+    """
     while True:
         try:
-            data, addr = sock.recvfrom(1024)
-            print("oiiiii")
-            encrypted_message, received_time = pickle.loads(data)
-            print("oi", encrypt_message)
+            data, addr = sock.recvfrom(1024)  # Recebe dados de outros usuários.
+            encrypted_message, received_time = pickle.loads(data)  # Desempacota e descriptografa a mensagem.
             decrypted_message = decrypt_message(key, encrypted_message)
-            print("oi", decrypted_message)
-            message = json.loads(decrypted_message)
-            print("oi2")
-            clock.update(received_time)
-            print("oi")
-            print(f"{message['alias']} disse: {message['text']} at Lamport time {clock.value}")
+            message = json.loads(decrypted_message)  # Converte a mensagem de JSON para um dicionário Python.
+            clock.update(received_time)  # Atualiza o relógio de Lamport com o tempo recebido.
+            print(f"{message['alias']} disse: {message['text']}")  # Exibe a mensagem.
         except Exception as e:
             print(f"Erro ao receber mensagem: {e}")
 
 def send_message(sock, clock, alias, peers, key):
+    """
+    Função para enviar mensagens para outros usuários. As mensagens são criptografadas antes do envio.
+    """
     while True:
-        message_text = input("Digite sua mensagem: ")
-        lamport_time = clock.increment()
-        message_data = json.dumps({"alias": alias, "text": message_text})
-        encrypted_data = encrypt_message(key, message_data)
-        data = pickle.dumps((encrypted_data, lamport_time))
+        message_text = input("Digite sua mensagem: ")  # Solicita uma mensagem do usuário.
+        lamport_time = clock.increment()  # Incrementa o relógio de Lamport.
+        message_data = json.dumps({"alias": alias, "text": message_text})  # Prepara a mensagem.
+        encrypted_data = encrypt_message(key, message_data)  # Criptografa a mensagem.
+        data = pickle.dumps((encrypted_data, lamport_time))  # Empacota a mensagem para envio
+
         for peer in peers:
+            # Envia a mensagem criptografada para cada peer na lista.
             sock.sendto(data, peer)
 
 def main():
-
+    """
+    Função principal para executar o chat.
+    Configura o chat, incluindo validação de chave, criação de socket e início das threads de escuta e envio.
+    """
+    # Solicita ao usuário um apelido para o chat.
     alias = input("Digite seu apelido no chat: ")
 
+    # Loop para obter uma chave de criptografia válida do usuário.
+    while True:
+        key_input = input("Digite uma chave de segurança (mínimo de 8 caracteres, incluindo letras, números e símbolos): ")
+        if validate_key(key_input):
+            break
+        else:
+            print("Chave inválida. Por favor, siga o formato solicitado.")
+
+    # Gera uma chave Fernet válida a partir da entrada do usuário.
+    key = generate_fernet_key_from_input(key_input)
+
+    # Lista para armazenar os endereços IP dos outros membros do chat.
     peers = []
+    # Pede ao usuário o número de membros no chat.
     num = int(input("Digite o número de membros que terá seu chat em grupo (sem incluir você): "))
     for _ in range(num):
+        # Pede o IP de cada membro do chat.
         ip = input("Digite o IP do membro: ")
+        # Adiciona o IP e a porta à lista de peers.
         peers.append((ip, numIp))
 
+    # Imprime uma linha em branco para separar a configuração da interação do chat.
+    print("\n")
+    # Configura um socket UDP para comunicação.
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    # Associa o socket à porta especificada.
     sock.bind(('', numIp))
 
+    # Cria uma instância do relógio de Lamport.
     clock = LamportClock()
-
-    key = generate_key()
-    print("Key ", key)
-
+    
+    # Inicia uma thread para ouvir mensagens de entrada.
     listener_thread = threading.Thread(target=listen_for_messages, args=(sock, clock, alias, key), daemon=True)
     listener_thread.start()
 
+    # Chama a função para enviar mensagens. Esta função continuará em execução e permitirá que o usuário
+    # digite e envie mensagens.
     send_message(sock, clock, alias, peers, key)
 
 if __name__ == "__main__":
-    main()
+    main()  # Executa a função principal se o script for o módulo principal.
